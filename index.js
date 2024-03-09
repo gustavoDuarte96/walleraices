df1c521bf6d3860ec2dae70f0e1c95cd53a40228
Gustavo Duarte <gd245149@fi365.ort.edu.uy>
2024-03-09 01:06:52 -0300

    Section 7 OK

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-const express = require('express');
+import express from 'express';
+import usuarioRoutes from './routes/usuarioRoutes.js';
 
 // Crear la app
 const app = express();
+
+// Routing
+ app.use('/auth', usuarioRoutes);
+
+ //Habilitar pug
+ app.set('view engine', 'pug');
+ app.set('views', './views');
+
+ // Carpeta Pública
+ app.use(express.static('public'));
 
 // Definir un puerto
 const port = 3000;
